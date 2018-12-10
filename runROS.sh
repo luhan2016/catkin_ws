da81bda21d926f0f920245413d977daa7ac4cb55
luhan20016 <luhansophia@gmail.com>
2018-12-10 09:26:34 -0800

    first changed code according to the new pseudo code, but the green car got stuck before enter the intersection

--- runROS.sh
+++ runROS.sh
@@ -4,5 +4,5 @@ cd ~/catkin_ws
 catkin_make
 source devel/setup.bash
 
-roslaunch virtual_blinker sim.launch >> output/20181209output4.txt
+roslaunch virtual_blinker sim.launch >> output/20181210output3.txt
 
