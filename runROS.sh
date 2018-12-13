c055db4b7e6aac7730b3ce3e192266afceb63c5e
luhan20016 <luhansophia@gmail.com>
2018-12-13 12:12:05 -0800

    always stop half way

--- runROS.sh
+++ runROS.sh
@@ -4,5 +4,5 @@ cd ~/catkin_ws
 catkin_make
 source devel/setup.bash
 
-roslaunch virtual_blinker sim.launch >> output/20181210output3.txt
+roslaunch virtual_blinker sim.launch >> output/20181213output3.txt
 
